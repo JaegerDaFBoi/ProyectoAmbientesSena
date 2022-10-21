746e14834d7224155e168c87a8cff83c859ecf34
JaegerDaFBoi <mcallister2696@gmail.com>
2022-10-21 18:44:25 -0500

    pagina principal diseñada

--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -2,12 +2,14 @@
 
 @section('title', 'Ambientes Sena')
 
-@section('content_header')
-    <h1>Pagina Principal</h1>
-@stop
+
 
 @section('content')
-    <p>Aqui va la pagina principal</p>
+<div class="row m-0 justify-content-center vh-100 align-items-start" style="background-image: url({{ asset('img/imgpaginaprincipal.jpg') }}); background-size: cover">
+    <div class="col-auto text-center bg-gradient-orange border rounded mt-2">
+        <h1 class="text-navy" style="font-size: 5em">Bienvenido a Ambientes SENA</h1>
+    </div>
+</div>
 @stop
 
 
